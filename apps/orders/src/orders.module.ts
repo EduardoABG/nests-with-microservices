feef05c39fa380c033b07a55a2f61e92f6c594de
eduardo <eduardo_abg@hotmail.com>
2024-06-26 11:09:19 -0300

    chore: module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { PrismaService } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [PrismaService],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
